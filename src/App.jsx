3106f0f2a40675677e5a0f026b8befc1475d5d07
musakazim989 <musakazim989@gmail.com>
2023-07-11 23:10:15 +0600

    props added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TextDisplay from "./components/TextDisplay"
+
+function App() {
+  return (
+    <>
+      <TextDisplay text="i'm writing my text shown by props" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TextDisplay.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function TextDisplay({ text }) {
+  return <p>{text}</p>
+}
